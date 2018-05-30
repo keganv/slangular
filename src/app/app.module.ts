af532e534c0b7a3e23c27b2d80725a2e38fa5dda
Kegan VanSickle <keganv@keganv.com>
2018-05-29 20:34:36 -0500

    - Importing dependencies and updating Module arguments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatExpansionModule, MatListModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { IpDataService } from './ipdata.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatExpansionModule,
+    MatListModule,
   ],
-  providers: [],
+  providers: [
+    IpDataService
+  ],
+  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
