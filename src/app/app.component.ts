8395a75700350c926cd5e650add8462c66dda21f
Kegan VanSickle <keganv@keganv.com>
2018-05-29 20:33:32 -0500

    - Shows and sets the IPs and associated Subnets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { IpData, IpDataService } from './ipdata.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ IpDataService ]
 })
-
 export class AppComponent {
-  title = 'Dashboard';
+  title  = 'Dashboard';
+  ipData = [];
+  subnet = null;
+
+  constructor(private ipDataService: IpDataService) { }
+
+  showData(): void {
+    this.ipData = []; // Reset the array for each call
+    this.ipDataService.getData().subscribe(
+      (data: IpData[]) => this.ipData = data
+    );
+  }
+
+  setSubnet(mainIp, ipInfo): void {
+    this.subnet = {
+      'ip': mainIp,
+      'subnet': ipInfo.ip,
+      'address': ipInfo.address_tag
+    };
+  }
 }
